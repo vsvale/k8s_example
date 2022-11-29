61e04af65b4a26e4782af466be98d45e05935986
dataeng <viniciusdvale@gmail.com>
2022-11-29 18:44:47 -0300

    stream example

---
+++ code/spark/example/silver/dimproductcategory-silver.py
@@ -0,0 +1,101 @@
+# import libraries
+from delta.tables import DeltaTable
+from pyspark.sql import SparkSession
+from pyspark import SparkConf
+from pyspark.sql.functions import current_timestamp, current_date, col, lit, when
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, TimestampType, DateType
+
+# main spark program
+# init application
+if __name__ == '__main__':
+
+    # init session
+    # set configs
+    spark = SparkSession \
+        .builder \
+        .appName("example-dimproductcategory-silver-py") \
+        .config("spark.hadoop.fs.s3a.endpoint", "http://172.19.0.2:8686") \
+        .config("spark.hadoop.fs.s3a.access.key", "yZe8zwSjfmt0GcJJ") \
+        .config("spark.hadoop.fs.s3a.secret.key", "O5IgFtO5Qjddzec8N2gL8LD3qu5r3CLG") \
+        .config("spark.hadoop.fs.s3a.path.style.access", True) \
+        .config("spark.hadoop.fs.s3a.fast.upload", True) \
+        .config("spark.hadoop.fs.s3a.multipart.size", 104857600) \
+        .config("fs.s3a.connection.maximum", 100) \
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+        .config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .getOrCreate()
+
+    # show configured parameters
+    print(SparkConf().getAll())
+
+    # set log level
+    spark.sparkContext.setLogLevel("INFO")
+
+    # variables
+    bronze_table = "s3a://lakehouse/bronze/example/productcategory/"
+    destination_folder = "s3a://lakehouse/silver/example/dimproductcategory/"
+    write_delta_mode = "overwrite"
+    # read bronze data
+
+    bronze_df = spark.read.format("delta").load(bronze_table)
+    bronze_df = bronze_df.alias("b")
+
+
+    silver_table = (
+        bronze_df
+        .select(
+            col("b.ProductCategoryID").alias("ProductCategoryKey"),
+            col("b.ProductCategoryID").alias("ProductCategoryAlternateKey"),
+            col("b.name").alias("EnglishProductCategoryName"),
+            lit(None).alias("SpanishProductCategoryName"),
+            lit(None).alias("FrenchProductCategoryName")
+    )
+    )
+
+    silver_table = silver_table.withColumn("s_create_at", current_timestamp())
+    silver_table = silver_table.withColumn("s_load_date", current_date())
+
+   
+    if DeltaTable.isDeltaTable(spark, destination_folder):
+        dt_table = DeltaTable.forPath(spark, destination_folder)
+        dt_table.alias("historical_data")\
+            .merge(
+                silver_table.alias("new_data"),
+                '''
+                historical_data.CustomerID = new_data.CustomerID 
+                ''')\
+            .whenMatchedUpdateAll()\
+            .whenNotMatchedInsertAll()
+    else:
+        DeltaTable.createIfNotExists(spark) \
+        .tableName("dimproductcategory") \
+        .addColumn("ProductCategoryKey", IntegerType()) \
+        .addColumn("ProductCategoryAlternateKey", IntegerType()) \
+        .addColumn("EnglishProductCategoryName", StringType()) \
+        .addColumn("SpanishProductCategoryName", StringType()) \
+        .addColumn("FrenchProductCategoryName", StringType()) \
+        .location(destination_folder) \
+        .execute()
+
+        silver_table.write.mode(write_delta_mode)\
+            .option("mergeSchema", "true")\
+            .format("delta")\
+            .partitionBy("s_load_date")\
+            .save(destination_folder)
+
+    #verify count origin vs destination
+    origin_count = silver_table.count()
+
+    destiny = spark.read \
+        .format("delta") \
+        .load(destination_folder)
+    
+    destiny_count = destiny.count()
+
+    if origin_count != destiny_count:
+        raise AssertionError("Counts of origin and destiny are not equal")
+
+    # stop session
+    spark.stop()
---
+++ code/spark/example/stream/dimcurrency-landing.py
@@ -0,0 +1,64 @@
+# import libraries
+from settings import *
+from delta.tables import DeltaTable
+from pyspark.sql import SparkSession
+from schemas import schemadimcurrency
+from pyspark.sql.functions import *
+
+# main spark program
+if __name__ == '__main__':
+
+    # init spark session
+    spark = SparkSession \
+            .builder \
+            .appName("dimcurrency-stream") \
+            .config("spark.hadoop.fs.s3a.endpoint", S3ENDPOINT) \
+            .config("spark.hadoop.fs.s3a.access.key", S3ACCESSKEY) \
+            .config("spark.hadoop.fs.s3a.secret.key", S3SECRETKEY) \
+            .config("spark.hadoop.fs.s3a.path.style.access", True) \
+            .config("spark.hadoop.fs.s3a.fast.upload", True) \
+            .config("spark.hadoop.fs.s3a.multipart.size", 104857600) \
+            .config("fs.s3a.connection.maximum", 100) \
+            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+            .config("spark.delta.logStore.class", "org.apache.spark.sql.delta.storage.S3SingleDriverLogStore") \
+            .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+            .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+            .config("spark.sql.streaming.kafka.useDeprecatedOffsetFetching","false") \
+            .getOrCreate()
+
+    # set log level to info
+    # [INFO] or [WARN] for more detailed logging info
+    spark.sparkContext.setLogLevel("INFO")
+
+    # refer to schema.py file
+    schema = schemadimcurrency
+    
+    origin_folder = "s3a://landing/example/dw-files/currency"
+
+    destination_topic = "dimcurrency_spark_stream_dwfiles"
+
+
+    landing_table = spark.readStream.options(header='False',delimiter=',').csv(origin_folder, schema=schema)
+    landing_table.printSchema()
+    print(landing_table.isStreaming)
+    # trigger using processing time = interval of the micro-batches
+    # formatting to deliver to apache kafka payload (value)
+    write_into_topic = landing_table \
+        .selectExpr("CAST(trim(CurrencyKey) AS STRING) AS key", "to_json(struct(*)) AS value") \
+        .writeStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", BOOTSTRAP_SERVERS) \
+        .option("topic", destination_topic) \
+        .option("checkpointLocation", "checkpoint") \
+        .outputMode("append") \
+        .trigger(processingTime="120 seconds") \
+        .start()  \
+        
+    # monitoring streaming queries
+    # structured streaming output info
+    # read last progress & last status of query
+    print(write_into_topic.lastProgress)
+    print(write_into_topic.status)
+
+    # block until query is terminated
+    write_into_topic.awaitTermination()
