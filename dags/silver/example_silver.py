c2ed82be261bf164d1e17fd3dbea9588907d570c
dataeng <viniciusdvale@gmail.com>
2022-11-30 09:53:24 -0300

    update example silver

--- dags/silver/example_silver.py
+++ dags/silver/example_silver.py
@@ -87,93 +87,6 @@ def example_silver():
         [verify_customer_bronze,verify_customeraddress_bronze,verify_address_bronze] >> silver_dimcustomer_spark_operator >> monitor_silver_dimcustomer_spark_operator >> list_silver_example_dimcustomer_folder
 
     @task_group()
-    def dimgeography_silver():
-        # verify if new data has arrived on bronze bucket
-        verify_address_bronze = S3KeySensor(
-        task_id='t_verify_address_bronze',
-        bucket_name=LAKEHOUSE,
-        bucket_key='bronze/example/address/*/*.parquet',
-        wildcard_match=True,
-        timeout=18 * 60 * 60,
-        poke_interval=120,
-        aws_conn_id='minio')
-
-        # verify if new data has arrived on silver bucket
-        verify_dimsalesterritory_silver = S3KeySensor(
-        task_id='t_verify_dimsalesterritory_silver',
-        bucket_name=LAKEHOUSE,
-        bucket_key='silver/example/dimsalesterritory/*/*.parquet',
-        wildcard_match=True,
-        timeout=18 * 60 * 60,
-        poke_interval=120,
-        aws_conn_id='minio')
-
-
-        # use spark-on-k8s to operate against the data
-        silver_dimgeography_spark_operator = SparkKubernetesOperator(
-        task_id='t_silver_dimgeography_spark_operator',
-        namespace='processing',
-        application_file='example-dimgeography-silver.yaml',
-        kubernetes_conn_id='kubeconnect',
-        do_xcom_push=True)
-
-        # monitor spark application using sensor to determine the outcome of the task
-        monitor_silver_dimgeography_spark_operator = SparkKubernetesSensor(
-        task_id='t_monitor_silver_dimgeography_spark_operator',
-        namespace="processing",
-        application_name="{{ task_instance.xcom_pull(task_ids='dimgeography_silver.t_silver_dimgeography_spark_operator')['metadata']['name'] }}",
-        kubernetes_conn_id="kubeconnect")
-
-        # Confirm files are created
-        list_silver_example_dimgeography_folder = S3ListOperator(
-        task_id='t_list_silver_example_dimgeography_folder',
-        bucket=LAKEHOUSE,
-        prefix='silver/example/dimgeography',
-        delimiter='/',
-        aws_conn_id='minio',
-        do_xcom_push=True)    
-
-        [verify_address_bronze,verify_dimsalesterritory_silver] >> silver_dimgeography_spark_operator >> monitor_silver_dimgeography_spark_operator >> list_silver_example_dimgeography_folder
-
-    @task_group()
-    def dimsalesterritory_silver():
-        # verify if new data has arrived on bronze bucket
-        verify_address_bronze = S3KeySensor(
-        task_id='t_verify_address_bronze',
-        bucket_name=LAKEHOUSE,
-        bucket_key='bronze/example/address/*/*.parquet',
-        wildcard_match=True,
-        timeout=18 * 60 * 60,
-        poke_interval=120,
-        aws_conn_id='minio')
-
-        # use spark-on-k8s to operate against the data
-        silver_dimsalesterritory_spark_operator = SparkKubernetesOperator(
-        task_id='t_silver_dimsalesterritory_spark_operator',
-        namespace='processing',
-        application_file='example-dimsalesterritory-silver.yaml',
-        kubernetes_conn_id='kubeconnect',
-        do_xcom_push=True)
-
-        # monitor spark application using sensor to determine the outcome of the task
-        monitor_silver_dimsalesterritory_spark_operator = SparkKubernetesSensor(
-        task_id='t_monitor_silver_dimsalesterritory_spark_operator',
-        namespace="processing",
-        application_name="{{ task_instance.xcom_pull(task_ids='dimsalesterritory_silver.t_silver_dimsalesterritory_spark_operator')['metadata']['name'] }}",
-        kubernetes_conn_id="kubeconnect")
-
-        # Confirm files are created
-        list_silver_example_dimsalesterritory_folder = S3ListOperator(
-        task_id='t_list_silver_example_dimsalesterritory_folder',
-        bucket=LAKEHOUSE,
-        prefix='silver/example/dimsalesterritory',
-        delimiter='/',
-        aws_conn_id='minio',
-        do_xcom_push=True)    
-
-        verify_address_bronze >> silver_dimsalesterritory_spark_operator >> monitor_silver_dimsalesterritory_spark_operator >> list_silver_example_dimsalesterritory_folder
-    
-    @task_group()
     def dimcurrency_silver():
         # use spark-on-k8s to operate against the data
         silver_dimcurrency_spark_operator = SparkKubernetesOperator(
@@ -200,7 +113,7 @@ def example_silver():
         do_xcom_push=True)
 
         silver_dimcurrency_spark_operator >> monitor_silver_dimcurrency_spark_operator >> list_silver_example_dimcurrency_folder
-    dimsalesterritory_silver() >> dimgeography_silver() >> dimcustomer_silver()
+    dimcustomer_silver()
     dimcurrency_silver()
 
 
