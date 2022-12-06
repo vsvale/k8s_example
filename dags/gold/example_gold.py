8aa4d6c8d0225c285a85d4b01aeb73c8a8f63bd6
dataeng <viniciusdvale@gmail.com>
2022-12-06 09:49:35 -0300

    test dag gold productcategoru

--- dags/gold/example_gold.py
+++ dags/gold/example_gold.py
@@ -59,7 +59,6 @@ def example_gold():
 
         gold_dimsalesterritory_spark_operator >> monitor_gold_dimsalesterritory_spark_operator >> list_gold_example_dimsalesterritory_folder
 
-
     @task_group()
     def dimproductsubcategory_gold():
         # verify if new data has arrived on silver
@@ -97,5 +96,45 @@ def example_gold():
         do_xcom_push=True)
         
         verify_dimproductsubcategory_silver >> gold_dimproductsubcategory_spark_operator >> monitor_gold_dimproductsubcategory_spark_operator >> list_gold_example_dimproductsubcategory_folder
-    [dimsalesterritory_gold(),dimproductsubcategory_gold()]           
+
+    @task_group()
+    def dimproductcategory_gold():
+        # verify if new data has arrived on silver
+        verify_dimproductcategory_silver = S3KeySensor(
+        task_id='t_verify_dimproductcategory_silver',
+        bucket_name=LAKEHOUSE,
+        bucket_key='silver/example/dimproductcategory/*.parquet',
+        wildcard_match=True,
+        timeout=18 * 60 * 60,
+        poke_interval=120,
+        aws_conn_id='minio')
+
+        # use spark-on-k8s to operate against the data
+        gold_dimproductcategory_spark_operator = SparkKubernetesOperator(
+        task_id='t_gold_dimproductcategory_spark_operator',
+        namespace='processing',
+        application_file='example-dimproductcategory-gold.yaml',
+        kubernetes_conn_id='kubeconnect',
+        do_xcom_push=True)
+
+        # monitor spark application using sensor to determine the outcome of the task
+        monitor_gold_dimproductcategory_spark_operator = SparkKubernetesSensor(
+        task_id='t_monitor_gold_dimproductcategory_spark_operator',
+        namespace="processing",
+        application_name="{{ task_instance.xcom_pull(task_ids='dimproductcategory_gold.t_gold_dimproductcategory_spark_operator')['metadata']['name'] }}",
+        kubernetes_conn_id="kubeconnect")
+
+        # Confirm files are created
+        list_gold_example_dimproductcategory_folder = S3ListOperator(
+        task_id='t_list_gold_example_dimproductcategory_folder',
+        bucket=LAKEHOUSE,
+        prefix='gold/example/dimproductcategory',
+        delimiter='/',
+        aws_conn_id='minio',
+        do_xcom_push=True)
+        
+        verify_dimproductcategory_silver >> gold_dimproductcategory_spark_operator >> monitor_gold_dimproductcategory_spark_operator >> list_gold_example_dimproductcategory_folder
+
+    [dimsalesterritory_gold()]
+    dimproductcategory_gold() >> dimproductsubcategory_gold()           
 dag = example_gold()
