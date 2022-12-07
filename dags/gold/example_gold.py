1355c1a5768dd86cba1b1067c73dc3390f5b5513
dataeng <viniciusdvale@gmail.com>
2022-12-07 19:35:47 -0300

    list s3 operator fix

--- dags/gold/example_gold.py
+++ dags/gold/example_gold.py
@@ -6,7 +6,7 @@ from os import getenv
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
 from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
-from airflow.providers.amazon.aws.operators.s3_list import S3ListOperator
+from airflow.providers.amazon.aws.operators.s3 import S3ListOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from astronomer.providers.amazon.aws.sensors.s3 import S3PrefixSensorAsync
 from astro import sql as aql
