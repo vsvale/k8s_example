df57e2a73f2d8d757fb4543bbde4c70dafd5497a
dataeng <viniciusdvale@gmail.com>
2022-12-12 08:50:01 -0300

    back from s3 to yu

--- dags/gold/example_gold.py
+++ dags/gold/example_gold.py
@@ -152,29 +152,10 @@ def example_gold():
         poke_interval=120,
         aws_conn_id='minio')
 
-        loads_s3_to_temp = aql.load_file(
-        task_id="t_loads_s3_to_temp",
+        loads_s3_to_yugabytedb = aql.load_file(
+        task_id="t_loads_s3_to_yugabytedb",
         input_file=File(path="s3://landing/example/dw-files/internetsalesreason/factinternetsalesreason.csv", filetype=FileType.CSV, conn_id='minio'),
         output_table=Table(
-            name="factinternetsalesreason_temp",
-            conn_id='yugabytedb_ysql',
-            columns=[
-                sqlalchemy.Column("SalesOrderNumber", sqlalchemy.String(20), nullable=False, key="SalesOrderNumber"),
-                sqlalchemy.Column("SalesOrderLineNumber", sqlalchemy.Integer, nullable=False, key="SalesOrderLineNumber"),
-                sqlalchemy.Column("SalesReasonKey", sqlalchemy.Integer, nullable=False, key="SalesReasonKey")
-            ],
-            metadata=Metadata(schema="public",database="salesdw"),
-            temp=True,        
-        ),
-        if_exists="replace",
-        use_native_support=True,
-        columns_names_capitalization="original",
-        )
-
-
-        loads_to_yugabytedb = aql.merge(
-            task_id="t_merge_sales_reason",
-            target_table=Table(
             name="factinternetsalesreason",
             conn_id='yugabytedb_ysql',
             columns=[
@@ -182,37 +163,17 @@ def example_gold():
                 sqlalchemy.Column("SalesOrderLineNumber", sqlalchemy.Integer, nullable=False, key="SalesOrderLineNumber"),
                 sqlalchemy.Column("SalesReasonKey", sqlalchemy.Integer, nullable=False, key="SalesReasonKey")
             ],
-            metadata=Metadata(schema="public",database="salesdw"),
+            metadata=Metadata(schema="public",database="salesdw")
+        
         ),
-        source_table=loads_s3_to_temp,
-        target_conflict_columns=["SalesOrderNumber","SalesOrderLineNumber"],
-        columns=["SalesOrderNumber","SalesOrderLineNumber","SalesReasonKey"],
-        if_conflicts="update",
+        if_exists="replace",
+        use_native_support=True,
+        columns_names_capitalization="original",
         )
 
-
-#        loads_s3_to_yugabytedb = aql.load_file(
-#        task_id="t_loads_s3_to_yugabytedb",
-#        input_file=File(path="s3://landing/example/dw-files/internetsalesreason/factinternetsalesreason.csv", filetype=FileType.CSV, conn_id='minio'),
-#        output_table=Table(
-#            name="factinternetsalesreason",
-#            conn_id='yugabytedb_ysql',
-#            columns=[
-#                sqlalchemy.Column("SalesOrderNumber", sqlalchemy.String(20), nullable=False, key="SalesOrderNumber"),
-#                sqlalchemy.Column("SalesOrderLineNumber", sqlalchemy.Integer, nullable=False, key="SalesOrderLineNumber"),
-#                sqlalchemy.Column("SalesReasonKey", sqlalchemy.Integer, nullable=False, key="SalesReasonKey")
-#            ],
-#            metadata=Metadata(schema="public",database="salesdw")
-#        
-#        ),
-#        if_exists="replace",
-#        use_native_support=True,
-#        columns_names_capitalization="original",
-#        )
-#
         
         
-        sensor_landing_example_salesreason >> loads_s3_to_temp
+        sensor_landing_example_salesreason >> loads_s3_to_yugabytedb
     [dimsalesterritory_gold(), factinternetsalesreason_gold()]
     dimproductcategory_gold() >> dimproductsubcategory_gold()
 dag = example_gold()
